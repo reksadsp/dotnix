ba4ccb3e485968cfc941906ead3c6010031d7f6d
reksadsp <accounts@reksa.fr>
2025-10-05 16:52:55 +0200

    fixed flake

--- flake.nix
+++ flake.nix
@@ -1,23 +1,20 @@
-{ config, pkgs, ... }:
-
 {
-  home.username = "reksa";
-  home.homeDirectory = "/home/reksa";
+  description = "Config Nix flake (Arch Omarchy) with Home Manager";
 
-  nixpkgs.config.allowUnfree = true;
+  inputs = { 
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05"; 
+    home-manager.url = "github:nix-community/home-manager/release-24.05"; 
+    home-manager.inputs.nixpkgs.follows = "nixpkgs"; 
+  }; 
 
-  home.packages = with pkgs; [
-    unzip btop nnn vlc sshfs git zsh fzf ripgrep fd bat wget curl
-  ];
-
-  programs.zsh.enable = true;
-
-  programs.git = {
-    enable = true;
-    userName = "reksadsp";
-    userEmail = "accounts@reksa.fr";
-  };
-
-  home.stateVersion = "24.05";
+  outputs = { self, nixpkgs, home-manager, ... }: 
+ 
+  let system = "x86_64-linux"; 
+    pkgs = import nixpkgs { inherit system; }; 
+    in { 
+      homeConfigurations."reksa@panasonic" = home-manager.lib.homeManagerConfiguration { 
+      inherit pkgs; 
+      modules = [ ./home/home.nix ]; 
+    }; 
+  }; 
 }
-
