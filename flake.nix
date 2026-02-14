26ec9fa0473d8b4fe3e207dd770107b67475966d
reksadsp <accounts@reksa.fr>
2026-02-15 00:23:23 +0100

    Clear git cache

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Config Nix flake with Home Manager (Linux + macOS)";
 
   inputs = {
+<<<<<<< HEAD
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -48,3 +49,21 @@
   };
 } 
 
+=======
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+ 
+  let system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    in {
+      homeConfigurations."reksa@panasonic" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [ ./home/home.nix ];
+    };
+  };
+}
+>>>>>>> 0148107 (clean systemd ngrok WIP)
