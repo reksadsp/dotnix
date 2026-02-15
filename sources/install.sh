beefa9274b6e255c34acd3913e365922d715f8c4
reksadsp <accounts@reksa.fr>
2026-02-15 03:31:21 +0100

    add sources

---
+++ sources/install.sh
@@ -0,0 +1,120 @@
+#!/usr/bin/env bash
+OS = Linux
+
+# Setup
+# OScheck, root checks, pkg checks
+#
+# whoami ?
+echo "whoami"
+ME = (whoami)
+SH = (which bash)
+DNS = "8.8.8.8"
+SYS = (cat /proc/sys/kernel/hostname)
+ADROUTE = "198.168.1.0/24"
+NGROK=False #TODO: Make ngrok config independant from Hardware Target
+NCLOUD=False
+
+# Security checks
+is_root () {
+    return $(id -u)
+}
+#Sudo permission ?
+has_sudo() {
+    local prompt
+    prompt=$(sudo -nv 2>&1)
+    if [ $? -eq 0 ]; then
+    echo "has_sudo__pass_set"
+    elif echo $prompt | grep -q '^sudo:'; then
+    echo "has_sudo__needs_pass"
+    else
+    echo "no_sudo"
+    fi
+# root access:
+elevate_cmd () {
+    local cmd=$@
+    HAS_SUDO=$(has_sudo)
+    case "$HAS_SUDO" in
+    has_sudo__pass_set)
+        sudo $cmd
+        ;;
+    has_sudo__needs_pass)
+        echo "Please supply sudo password for the following command: sudo $cmd"
+        sudo $cmd
+        ;;
+    *)
+        echo "Please supply root password for the following command: su -c \"$cmd\""
+        su -c "$cmd"
+        ;;
+    esac
+}
+if is_root; then
+    echo "Error: need to call this script as a normal user, not as root!"
+    exit 1
+fi
+elevate_cmd which adduser
+
+# Required pkgs
+# curl, git, ssh,
+#
+# Check_dependencies:
+check_dependencies(){
+    #Declare list of dependencies
+    declare -Ag deps=([curl]='curl' [git]='git' [ssh]='ssh' [lsof]='lsof')
+    #Declare list of package managers and their usages
+    declare -Ag packman_list=([pacman]='pacman -Sy' [apt]='echo "deb http://deb.debian.org/debian buster-backports main contrib non-free" > /etc/apt/sources.list.d/buster-backports.list; apt update -y; apt install -y' [yum]='yum install -y epel-release; yum repolist -y; yum install -y')
+    
+    #Find the package manager on the system and install the package
+    install_deps(){
+        for packman in ${!packman_list[@]}
+        do
+            which $packman &>/dev/null && eval $(echo ${packman_list[$packman]} "$*")
+        done
+    }
+    #Find the missing packages from list of dependencies
+    declare -ag missing_deps=()
+    for pack in ${!deps[@]}
+    do
+        which $pack &>/dev/null || missing_deps+=(${deps[$pack]})
+    done
+    #Install missing dependencies
+    test -z ${missing_deps[0]} || install_deps ${missing_deps[@]} || fail "Dependencies could not provide"
+}
+
+# CURL installs
+# flakes, home-manager environment
+#
+# DSys NIX
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+# Tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+# Docker
+curl -fsSL https://get.docker.com | sudo sh
+# Enable flakes
+echo 'experimental-features = nix-command flakes' | sudo tee -a /etc/nix/nix.conf
+# Download Env Repository
+git clone git@github.com:reksadsp/dotnix.git ~/dotnix
+cd ~/dotnix
+# Switch env
+nix run home-manager/master -- switch --flake .#reksa@$SYS
+nix flake update
+nix run home-manager/master -- switch --flake .#reksa@$SYS
+
+# Sudo RUN
+# Linux init
+#
+if [[ $HAS_SUDO ]]; then
+  #SystemD
+  systemctl daemon reload
+  sudo systemctl start tailscaled
+  sudo tailscale up --accept-dns=true --dns=$DNS --accept-routes --advertise-routes=$ADROUTE
+  if [[ $NGROK ]]; then
+    #Ngrok
+    sudo chmod +x ./ngrok.sh
+    source ./ngrok.sh
+  fi
+  if [[ $NCLOUD]]; then
+    #NextCloud
+    sudo chmod +x ./nextcloud.sh
+    source ./nextcloud.sh
+  fi
+fi
